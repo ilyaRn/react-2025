cccb9454fadfa3ec258b912174c9fe49cf37c8a2
ilyaRn <void_rain@mail.ru>
2025-06-09 14:13:27 +0500

    hw-7

---
+++ src/redux/entities/users/slice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { users } from "../../../../materials/normalized-datas";
+
+const initialState = {
+  ids: users.map(({ id }) => id),
+  entities: users.reduce((acc, user) => {
+    acc[user.id] = user;
+    return acc;
+  }, {}),
+};
+
+export const userSlice = createSlice({
+  name: "userSlice",
+  initialState,
+  selectors: {
+    selectUserIds: (state) => state.ids,
+    selectUserById: (state, id) => state.entities[id] || {},
+    // TODO: memoize
+    selectUsersByIds: (state, ids) => ids.map((id) => state.entities[id] || {}),
+  },
+});
+
+export const { selectUserIds, selectUserById, selectUsersByIds } =  userSlice.selectors;
